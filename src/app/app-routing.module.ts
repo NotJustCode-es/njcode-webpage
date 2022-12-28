8afa2d5dabf2ca9e477944afaa2c70b2adcc2427
Jose Ignacio Rubio <jrubiosauceda@gmail.com>
2022-12-28 16:36:24 +0100

    build: upgrade to v14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabled',
+      initialNavigation: 'enabledBlocking',
       scrollPositionRestoration: 'enabled',
       relativeLinkResolution: 'corrected',
       onSameUrlNavigation: 'reload',
