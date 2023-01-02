80c43dda11048dc04eef445be9942c173372faa5
Jose Ignacio Rubio <jrubiosauceda@gmail.com>
2023-01-02 13:52:05 +0100

    fix(compiler): remove typeof of sections

--- src/app/core/models/sections.ts
+++ src/app/core/models/sections.ts
@@ -1,13 +1,10 @@
-import { SectionFooterModule } from '@sections/section-footer/section-footer.module';
-import { SectionNavbarModule } from '@sections/section-navbar/section-navbar.module';
-
 export const SECTIONS: Map<string, Function> = new Map([
   [
     'section-navbar',
-    (): Promise<typeof SectionNavbarModule> => import('@sections/section-navbar/section-navbar.module').then(m => m.SectionNavbarModule),
+    (): Promise<unknown> => import('@sections/section-navbar/section-navbar.module').then(m => m.SectionNavbarModule),
   ],
   [
     'section-footer',
-    (): Promise<typeof SectionFooterModule> => import('@sections/section-footer/section-footer.module').then(m => m.SectionFooterModule),
+    (): Promise<unknown> => import('@sections/section-footer/section-footer.module').then(m => m.SectionFooterModule),
   ],
 ]);
