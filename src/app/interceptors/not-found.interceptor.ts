aae888374392b9bdd508dbb4e779ca6c07473222
marcNJC <Marc.torres@njcode.es>
2023-01-04 20:04:59 +0100

    feat: implemented interceptor for error 404

---
+++ src/app/interceptors/not-found.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {Router} from "@angular/router"; 
+
+@Injectable()
+export class NotFoundInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (error.status === 404) {
+          this.router.navigateByUrl('notfound', { replaceUrl: true });
+          return EMPTY;
+        } else {
+          return throwError(() => new Error(error));
+        }
+      }
+      )
+    );
+  }
+}
