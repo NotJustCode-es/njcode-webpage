3605e1bf7a48f6150137df66075d77720d15cf5a
Jose Ignacio Rubio <jrubiosauceda@gmail.com>
2023-01-06 17:48:10 +0100

    feat(i18n): create i18n service to handle routing and locales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouteReuseStrategy } from '@angular/router';
+import { CoreModule } from '@core/core.module';
+import { DynamicRouteReuseStrategy } from '@core/strategies/dynamic-route-reuse.strategy';
 import { BrowserStateInterceptor } from '@interceptors/browser-state.interceptor';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,9 +15,12 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'NJCodeServerApp' }),
+    BrowserModule.withServerTransition({
+      appId: 'NJCodeServerApp',
+    }),
     TransferHttpCacheModule,
     BrowserAnimationsModule,
+    CoreModule,
     AppRoutingModule,
     HttpClientModule,
   ],
@@ -23,6 +29,10 @@ import { AppComponent } from './app.component';
       provide: HTTP_INTERCEPTORS,
       useClass: BrowserStateInterceptor,
       multi: true,
+    },
+    {
+      provide: RouteReuseStrategy,
+      useClass: DynamicRouteReuseStrategy,
     },
   ],
   bootstrap: [
