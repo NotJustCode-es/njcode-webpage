f0d8ec9f39a31d58314c9ec38341530121c96971
marcNJC <Marc.torres@njcode.es>
2023-01-15 18:38:24 +0100

    feat: added testing to title.service

--- src/app/core/services/title.service.spec.ts
+++ src/app/core/services/title.service.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
 import { TitleService } from './title.service';
 
 describe('TitleService', () => {
+  let title: Title;
   let service: TitleService;
 
   beforeEach(() => {
@@ -11,5 +13,19 @@ describe('TitleService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Page title should be upgraded', () => {
+    const newTitle = 'newTitulo';
+    service.setTitle(newTitle);
+    title = TestBed.inject(Title);
+    expect(title.getTitle()).toBe(newTitle);
+  });
+
+  it('When undefined title is given, title should be default title NJCode ', () => {
+    const defaultTitle = 'NJCode';
+    service.setTitle(undefined);
+    title = TestBed.inject(Title);
+    expect(title.getTitle()).toBe(defaultTitle);
   });
 });
