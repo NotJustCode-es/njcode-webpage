63fc29ae60485934dcd46e6844cac2ebb87a137f
marcNJC <Marc.torres@njcode.es>
2023-01-19 19:44:16 +0100

    feat: created TypeMetadataFields model

--- server/models/contentful-content-types/page.ts
+++ server/models/contentful-content-types/page.ts
@@ -1,11 +1,12 @@
 import * as Contentful from 'contentful';
+import { TypeMetadataFields } from './metadata';
 
 export interface TypePageFields {
   name?: Contentful.EntryFields.Symbol;
   title?: Contentful.EntryFields.Symbol;
   slug: Contentful.EntryFields.Symbol;
   sections?: Contentful.Entry<Record<string, unknown>>[];
-  metadata?: Contentful.Entry<Record<string, unknown>>;
+  metadata?: Contentful.Entry<TypeMetadataFields>;
 }
 
 export type TypePage = Contentful.Entry<TypePageFields>;
