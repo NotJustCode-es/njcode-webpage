351714a8555d550c9b589ab241f88bea6bace4a6
marcNJC <marc.torres@njcode.es>
2023-02-13 19:10:41 +0100

    feat: mapped section proyectos to module

--- src/app/core/models/sections.ts
+++ src/app/core/models/sections.ts
@@ -15,4 +15,8 @@ export const SECTIONS: Map<string, Function> = new Map([
     'section-footer',
     (): Promise<unknown> => import('@sections/section-footer/section-footer.module').then(m => m.SectionFooterModule),
   ],
+  [
+    'section-proyectos',
+    (): Promise<unknown> => import('@sections/section-proyectos/section-proyectos.module').then(m => m.SectionProyectosModule),
+  ],
 ]);
