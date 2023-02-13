d015faeb58c671d8ba41c1d11e654891ba8b48b3
marcNJC <marc.torres@njcode.es>
2023-02-13 10:19:54 +0100

    feat: added client card type

---
+++ server/models/contentful-content-types/client-card.ts
@@ -0,0 +1,13 @@
+import { TypeRouteLinkFields } from '@server/models/contentful-content-types/route-link';
+import * as Contentful from 'contentful';
+
+export interface TypeClient__cardFields {
+  name: Contentful.EntryFields.Symbol;
+  clientName: Contentful.EntryFields.Symbol;
+  position: Contentful.EntryFields.Symbol;
+  description: Contentful.EntryFields.Symbol;
+  socialMedia?: Contentful.Entry<TypeRouteLinkFields>[];
+  image: Contentful.EntryFields.Symbol;
+}
+
+export type TypeClient__card = Contentful.Entry<TypeClient__cardFields>;
