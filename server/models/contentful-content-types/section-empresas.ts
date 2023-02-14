67296b8b4010b0dddda7cfe2046ac330c32b2776
marcNJC <marc.torres@njcode.es>
2023-02-14 14:43:02 +0100

    refactor: changed name of content to english

--- server/models/contentful-content-types/section-empresas.ts
+++
@@ -1,9 +0,0 @@
-import * as Contentful from 'contentful';
-
-export interface TypeSection__empresasFields {
-  name: Contentful.EntryFields.Symbol;
-  title: Contentful.EntryFields.Symbol;
-  empresas: Contentful.Asset[];
-}
-
-export type TypeSection__empresas = Contentful.Entry<TypeSection__empresasFields>;
