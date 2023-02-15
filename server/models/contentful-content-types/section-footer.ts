8dec2946d4ead3b07069ed8357a28f1551eeb459
marcNJC <marc.torres@njcode.es>
2023-02-15 13:00:09 +0100

    refactor: updated footer component

--- server/models/contentful-content-types/section-footer.ts
+++ server/models/contentful-content-types/section-footer.ts
@@ -1,7 +1,17 @@
+import { TypeLogoFields } from '@server/models/contentful-content-types/logo';
+import { TypeRouteLinkFields } from '@server/models/contentful-content-types/route-link';
 import * as Contentful from 'contentful';
 
 export interface TypeSection__footerFields {
   name: Contentful.EntryFields.Symbol;
+  subtitle: Contentful.EntryFields.Symbol;
+  rigths: Contentful.EntryFields.Symbol;
+  phoneTitle: Contentful.EntryFields.Symbol;
+  addressTitle: Contentful.EntryFields.Symbol;
+  phoneNumbers: Contentful.EntryFields.Symbol[];
+  adress: Contentful.EntryFields.Symbol;
+  logo: Contentful.Entry<TypeLogoFields>;
+  socialmedia: Contentful.Entry<TypeRouteLinkFields>[];
 }
 
 export type TypeSection__footer = Contentful.Entry<TypeSection__footerFields>;
