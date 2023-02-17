0750e46fcb5520123427bccfc33a664aa47473d3
marcNJC <marc.torres@njcode.es>
2023-02-17 12:53:07 +0100

    feat: added form controls and validation

--- src/app/sections/section-contact-form/section-contact-form.component.ts
+++ src/app/sections/section-contact-form/section-contact-form.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TypeSection__contact__formFields } from '@server/models/contentful-content-types/section-contact-form';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-section-contact-form',
@@ -9,4 +10,23 @@ import { TypeSection__contact__formFields } from '@server/models/contentful-cont
 })
 export class SectionContactFormComponent {
   @Input() data!: TypeSection__contact__formFields;
+
+  private readonly text = '[a-zA-Z]*';
+
+  private readonly email = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
+  contactForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.pattern(this.text), Validators.maxLength(100)]],
+    lastName: ['', [Validators.required, Validators.pattern(this.text), Validators.maxLength(100)]],
+    email: ['', [Validators.required, Validators.pattern(this.email), Validators.maxLength(256)]],
+    message: ['', [Validators.required, Validators.maxLength(256)]],
+  }, { updateOn: 'blur' });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    // TO DO: call mail endpoint
+    // eslint-disable-next-line no-console
+    console.warn(this.contactForm.value);
+  }
 }
--- src/app/sections/section-contact-form/section-contact-form.module.ts
+++ src/app/sections/section-contact-form/section-contact-form.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SectionContactFormComponent } from './section-contact-form.component';
 
 @NgModule({
@@ -8,6 +9,7 @@ import { SectionContactFormComponent } from './section-contact-form.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
   ],
   exports: [
     SectionContactFormComponent,
