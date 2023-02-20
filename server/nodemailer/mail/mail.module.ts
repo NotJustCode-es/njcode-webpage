8d8e4dc2b0a3bd1d5437e9494a8ff20f1554c7e1
marcNJC <marc.torres@njcode.es>
2023-02-20 16:49:22 +0100

    feat: created mail module

---
+++ server/nodemailer/mail/mail.module.ts
@@ -0,0 +1,37 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { MailController } from 'server/nodemailer/mail/mail.controller';
+import { MailService } from './mail.service';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
+      // or
+      transport: {
+        host: 'smtp.example.com',
+        secure: false,
+        auth: {
+          user: 'user@example.com',
+          pass: 'topsecret',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService], // 👈 export for DI
+  controllers: [MailController],
+})
+export class MailModule {}
