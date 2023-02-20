a48a5808a6a6d3d57305024eb5363a82bc1d4d86
marcNJC <marc.torres@njcode.es>
2023-02-20 16:48:05 +0100

    feat: updated app.module

--- server/app.module.ts
+++ server/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AngularUniversalModule } from '@nestjs/ng-universal';
 import { join } from 'path';
+import { MailModule } from './nodemailer/mail/mail.module';
 import { AppServerModule } from '../src/main.server';
 import { ContentfulApiModule } from './contentful-api/contentful-api.module';
 import configFactory from './core/config/config-factory';
@@ -24,6 +25,7 @@ const browserAppLocation = 'dist/njcode-webpage/browser';
       isGlobal: true,
     }),
     ContentfulApiModule,
+    MailModule,
   ],
 })
 export class AppModule { }
