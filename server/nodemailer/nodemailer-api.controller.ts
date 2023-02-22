6e2bfbdbdce9e0c8eba3724cdaf247c23cbb62e9
marcNJC <marc.torres@njcode.es>
2023-02-22 21:02:29 +0100

    feat: started using captcha to securize endpoint

--- server/nodemailer/nodemailer-api.controller.ts
+++ server/nodemailer/nodemailer-api.controller.ts
@@ -3,7 +3,9 @@ import {
   Controller,
   Inject,
   Post,
+  Headers,
 } from '@nestjs/common';
+import { Recaptcha } from '@nestlab/google-recaptcha';
 import { NodemailerApiService } from './nodemailer-api.service';
 import { MailParams } from '../models/mail-params';
 
@@ -13,13 +15,17 @@ export class NodemailerApiController {
     @Inject(NodemailerApiService)private readonly mailService: NodemailerApiService,
   ) {}
 
+  @Recaptcha()
   @Post('/send')
   async sendMail(
     @Body() params: MailParams,
+      @Headers('recaptcha') token: string,
   ): Promise<string> {
     // eslint-disable-next-line no-console
     console.log('Params on controller call: ', params);
+    // eslint-disable-next-line no-console
+    console.log('token on controller call: ', token);
 
-    return this.mailService.sendEmail(params);
+    return this.mailService.sendEmail(params, token);
   }
 }
