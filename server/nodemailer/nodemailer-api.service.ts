80cf5bbeb611438362a96a9f6cdd1fb983d4708c
marcNJC <marc.torres@njcode.es>
2023-02-22 21:02:29 +0100

    feat: started using captcha to securize endpoint

--- server/nodemailer/nodemailer-api.service.ts
+++ server/nodemailer/nodemailer-api.service.ts
@@ -1,12 +1,24 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Inject, Injectable } from '@nestjs/common';
+import { GoogleRecaptchaException, GoogleRecaptchaValidator } from '@nestlab/google-recaptcha';
 import { MailParams } from '../models/mail-params';
 
 @Injectable()
 export class NodemailerApiService {
-  constructor(@Inject(MailerService) private mailerService: MailerService) {}
+  constructor(
+    @Inject(MailerService) private mailerService: MailerService,
+    @Inject(GoogleRecaptchaValidator) private readonly recaptchaValidator: GoogleRecaptchaValidator,
+  ) {}
 
-  async sendEmail(params: MailParams): Promise<string> {
+  async sendEmail(params: MailParams, recaptchaToken: string): Promise<string> {
+    const result = await this.recaptchaValidator.validate({
+      response: recaptchaToken,
+    });
+
+    if (!result.success) {
+      throw new GoogleRecaptchaException(result.errors);
+    }
+
     const { name } = params;
     const { email } = params;
     const { message } = params;
