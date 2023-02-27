7bea6e49dcd835234401cdcfd6ea385a6a10787e
marcNJC <marc.torres@njcode.es>
2023-02-27 20:35:27 +0100

    refactor: changed decorator position

--- server/contact-api/contact-api.controller.ts
+++ server/contact-api/contact-api.controller.ts
@@ -11,13 +11,13 @@ import { Recaptcha } from '@nestlab/google-recaptcha';
 import { ContactApiService } from './contact-api.service';
 import { MailParams } from '../models/mail-params';
 
-@Recaptcha()
 @Controller('contact')
 export class ContactApiController {
   constructor(
     @Inject(ContactApiService)private readonly mailService: ContactApiService,
   ) {}
 
+  @Recaptcha()
   @Post('/send')
   @UsePipes(new ValidationPipe({ transform: true }))
   async sendMail(
