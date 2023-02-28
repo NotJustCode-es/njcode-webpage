2fd312a58f7ff53ac4ca6dbcea448f09a2aca3a3
marcNJC <marc.torres@njcode.es>
2023-02-28 13:18:34 +0100

    refactor: service should not validate token

--- server/contact-api/contact-api.service.ts
+++ server/contact-api/contact-api.service.ts
@@ -1,25 +1,14 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Inject, Injectable } from '@nestjs/common';
-import { GoogleRecaptchaException, GoogleRecaptchaValidator } from '@nestlab/google-recaptcha';
 import { MailParams } from '../models/mail-params';
 
 @Injectable()
 export class ContactApiService {
   constructor(
     @Inject(MailerService) private mailerService: MailerService,
-    @Inject(GoogleRecaptchaValidator) private readonly recaptchaValidator: GoogleRecaptchaValidator,
   ) {}
 
-  async sendEmail(params: MailParams, recaptchaToken: string): Promise<void> {
-    const result = await this.recaptchaValidator.validate({
-      response: recaptchaToken,
-      score: 0.8,
-      action: 'sendMail',
-    });
-
-    if (!result.success) {
-      throw new GoogleRecaptchaException(result.errors);
-    }
+  async sendEmail(params: MailParams): Promise<void> {
     const { name } = params;
     const { email } = params;
     const { message } = params;
