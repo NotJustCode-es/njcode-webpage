65e8eedea72eee26d578e965d3a0b4c6bf0a51d5
marcNJC <marc.torres@njcode.es>
2023-02-28 23:58:17 +0100

    feat: added testing for contact service

--- src/app/services/contact/contact.service.spec.ts
+++ src/app/services/contact/contact.service.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { ContactService } from './contact.service';
 
 describe('NodemailerService', () => {
   let service: ContactService;
+  let controller: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+    });
     service = TestBed.inject(ContactService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have correct config', () => {
+    service.sendMail('name', 'mail', 'message', 'token').subscribe(value => {
+      expect(value).toBeTruthy();
+    });
+    controller.expectOne(
+      { method: 'POST', url: '/api/contact/send/' },
+    );
   });
 });
