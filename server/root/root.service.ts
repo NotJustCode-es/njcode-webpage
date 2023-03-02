3cb61cba1790180cfefb08a8d09b0daf30c17210
marcNJC <marc.torres@njcode.es>
2023-03-02 18:26:03 +0100

    refactor: removed try catch and changed configuration

--- server/root/root.service.ts
+++ server/root/root.service.ts
@@ -8,40 +8,21 @@ import {
 
 @Injectable()
 export class RootService {
-  private xml!: Promise<Buffer>;
-
   async getSitemap(entries: EntryCollectionWithLinkResolutionAndWithUnresolvableLinks<TypePageFields>): Promise<Buffer> {
-    // this.params = { slug: 'asd', locale: 'awd', limit: 1000 };
+    const sitemapStream = new SitemapStream({
+      hostname: environment.hostUrl,
+    });
 
-    try {
-      const sitemapStream = new SitemapStream({
-        hostname: environment.hostUrl,
-      });
-      // const pipeline = sitemapStream.pipe(createGzip());
-
-      entries.items.forEach(entry => {
-        sitemapStream.write({
-          changefreq: EnumChangefreq.MONTHLY,
-          lastmod: entry.sys.updatedAt,
-          priority: 0.7,
-          url: entry.fields.slug,
-        } as SitemapItem);
-      });
-
-      // Add any other sitemap items for other pages of your site
+    entries.items.forEach(entry => {
       sitemapStream.write({
-        changefreq: EnumChangefreq.DAILY,
-        priority: 1,
-        url: '',
+        changefreq: EnumChangefreq.MONTHLY,
+        lastmod: entry.sys.updatedAt,
+        priority: 1.0,
+        url: entry.fields.slug,
       } as SitemapItem);
+    });
 
-      // Stream write the response
-      sitemapStream.end();
-      this.xml = streamToPromise(sitemapStream);
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(error);
-    }
-    return this.xml;
+    sitemapStream.end();
+    return streamToPromise(sitemapStream);
   }
 }
