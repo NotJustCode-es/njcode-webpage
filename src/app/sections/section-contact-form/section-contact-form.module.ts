a5e5dab649333e0c1b6d5b20a3f134b5b993f084
marcNJC <marc.torres@njcode.es>
2023-03-08 19:59:46 +0100

    feat: updated module to use ngalert

--- src/app/sections/section-contact-form/section-contact-form.module.ts
+++ src/app/sections/section-contact-form/section-contact-form.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { environment } from '@environments/environment';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { RecaptchaV3Module, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
 import { SectionContactFormComponent } from './section-contact-form.component';
 
@@ -19,6 +20,7 @@ import { SectionContactFormComponent } from './section-contact-form.component';
     CommonModule,
     ReactiveFormsModule,
     RecaptchaV3Module,
+    NgbAlertModule,
   ],
   exports: [
     SectionContactFormComponent,
