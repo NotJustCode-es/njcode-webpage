875d1b3675c4805ddc2ffc258d44d3fd0be88844
marcNJC <marc.torres@njcode.es>
2023-03-10 14:52:23 +0100

    feat: created interface to hold front variables

---
+++ server/models/front-configuration-params.ts
@@ -0,0 +1,8 @@
+export interface FrontConfigurationParams {
+  production: boolean;
+  default_lang: string;
+  available_lang: string[];
+  asset_url: string;
+  google_analytics_id: string;
+  google_recaptcha_site_key: string;
+}
