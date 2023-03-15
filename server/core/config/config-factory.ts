8d3b2dae41ae0da9d7d9de1e8cbc095685b0f242
marcNJC <marc.torres@njcode.es>
2023-03-15 12:17:02 +0100

    refactor: config-factory should include clientConfiguration

--- server/core/config/config-factory.ts
+++ server/core/config/config-factory.ts
@@ -8,4 +8,11 @@ export default (): Configuration => ({
     accessToken: process.env['CONTENTFUL_ACCESS_TOKEN'] || '',
     environment: process.env['CONTENTFUL_ENVIRONMENT'] || '',
   },
+  client: {
+    default_lang: process.env['DEFAULT_LANG'] || '',
+    available_lang: process.env['AVAILABLE_LANG']?.split(' ') || [''],
+    asset_url: process.env['ASSET_URL'] || '',
+    google_analytics_id: process.env['GOOGLE_ANALYTCS_ID'] || '',
+    google_recaptcha_site_key: process.env['GOOGLE_RECAPTCHA_SITE_KEY'] || '',
+  },
 });
--- server/core/models/configuration.ts
+++ server/core/models/configuration.ts
@@ -1,7 +1,9 @@
+import { ClientConfiguration } from '@server/models/client-configuration';
 import { ContentfulConfiguration } from './contentful-configuration';
 
 export interface Configuration {
   port: number;
   prefix: string;
   contentful: ContentfulConfiguration
+  client: ClientConfiguration
 }
