3c4f4921eeb45e143664a57e731def1f78c75c93
marcNJC <marc.torres@njcode.es>
2023-03-28 17:18:31 +0200

    feat: added testing to root

--- server/root/root.controller.spec.ts
+++ server/root/root.controller.spec.ts
@@ -3,11 +3,27 @@ import { ConfigService } from '@nestjs/config';
 import { contentfulApiStub } from '@server/shared/testing/stub/contentful-api.stub';
 import { ContentfulApiService } from '@server/contentful-api/contentful-api.service';
 import { configServiceStub } from '@server/shared/testing/stub/config-service.stub';
+import { Request, Response } from 'express';
+import { expect, jest } from '@jest/globals';
+import { ClientConfiguration } from '@server/core/models/client-configuration';
 import { RootService } from './root.service';
 import { RootController } from './root.controller';
 
 describe('ContactController', () => {
   let controller: RootController;
+  let service: RootService;
+  let configStub: ConfigService;
+
+  const requestMock = {
+    protocol: 'https',
+    get(): string {
+      return 'localhost:4200';
+    },
+  } as unknown as Request;
+
+  const responseMock = {
+    send: jest.fn(data => data),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,9 +43,27 @@ describe('ContactController', () => {
     }).compile();
 
     controller = module.get<RootController>(RootController);
+    service = module.get<RootService>(RootService);
+    configStub = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call sitemap', () => {
+    const serviceSpy = jest.spyOn(service, 'getSitemap');
+    controller.getEntries(requestMock).subscribe();
+    expect(serviceSpy).toHaveBeenCalledWith(expect.anything(), `${requestMock.protocol}://${requestMock.get('')}`);
+  });
+
+  it('should call robots', () => {
+    const serviceSpy = jest.spyOn(service, 'getRobotsContent');
+    controller.getRobots(responseMock, requestMock);
+    expect(serviceSpy).toHaveBeenCalledWith(requestMock.protocol, requestMock.get(''));
+  });
+
+  it('should return client configuration', () => {
+    expect(controller.getClientConfiguration()).toEqual(configStub.get<ClientConfiguration>('client'));
   });
 });
--- server/root/root.service.spec.ts
+++ server/root/root.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getTestSitemap } from '@server/shared/testing/snapshots/sitemap.snapshot.xml';
+import { createTestEntryCollection } from '@server/shared/testing/utils/contentful-entry-collection.util';
 import { RootService } from './root.service';
 
 describe('RootService', () => {
@@ -14,5 +16,21 @@ describe('RootService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('sitemap should generate proper xml', () => {
+    const originUrl = 'http://localhost:4200';
+    service.getSitemap(createTestEntryCollection(), originUrl)
+      .then(
+        data => {
+          expect(data.toString()).toBe(getTestSitemap());
+        },
+      );
+  });
+
+  it('should create valid robots', () => {
+    const protocol = 'https';
+    const origin = 'localhost:4200';
+    expect(service.getRobotsContent(protocol, origin)).toEqual(`User-agent: * \nDisallow: \nSitemap: ${protocol}://${origin}/sitemap.xml`);
   });
 });
