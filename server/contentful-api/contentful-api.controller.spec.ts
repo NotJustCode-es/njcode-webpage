f5a6d0ab92b699127866c13ec597454f038102ce
marcNJC <marc.torres@njcode.es>
2023-03-28 17:18:44 +0200

    feat: added testing to contentful-api

--- server/contentful-api/contentful-api.controller.spec.ts
+++ server/contentful-api/contentful-api.controller.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from '@nestjs/config';
 import { configServiceStub } from '@server/shared/testing/stub/config-service.stub';
+import { ContentfulPageQueryParams } from '@server/models/contentful-page-query-params';
+import { contentfulApiStub } from '@server/shared/testing/stub/contentful-api.stub';
 import { ContentfulApiService } from './contentful-api.service';
 import { ContentfulApiController } from './contentful-api.controller';
 
 describe('ContentfulApiController', () => {
   let controller: ContentfulApiController;
+  let service: ContentfulApiService;
+
+  const pageQueryParams = {
+    slug: 'test',
+  } as ContentfulPageQueryParams;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,13 +23,24 @@ describe('ContentfulApiController', () => {
         {
           provide: ConfigService,
           useClass: configServiceStub,
+        },
+        {
+          provide: ContentfulApiService,
+          useClass: contentfulApiStub,
         }],
     }).compile();
 
     controller = module.get<ContentfulApiController>(ContentfulApiController);
+    service = module.get<ContentfulApiService>(ContentfulApiService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call service getPage', async () => {
+    const serviceSpy = jest.spyOn(service, 'getPage');
+    controller.getPage(pageQueryParams);
+    expect(serviceSpy).toHaveBeenCalledWith(pageQueryParams);
   });
 });
--- server/shared/testing/stub/contentful-api.stub.ts
+++ server/shared/testing/stub/contentful-api.stub.ts
@@ -1,10 +1,14 @@
 import { TypePageFields } from '@server/models/contentful-content-types/page';
-import { createTestEntryCollection } from '@server/shared/testing/utils/contentful.utils';
+import { createTestEntryCollection } from '@server/shared/testing/utils/contentful-entry-collection.util';
 import { EntryCollectionWithLinkResolutionAndWithUnresolvableLinks } from 'contentful';
 import { Observable, of } from 'rxjs';
 
 export class contentfulApiStub {
   getAllPages(): Observable<EntryCollectionWithLinkResolutionAndWithUnresolvableLinks<TypePageFields>> {
+    return of(createTestEntryCollection());
+  }
+
+  getPage(): Observable<EntryCollectionWithLinkResolutionAndWithUnresolvableLinks<TypePageFields>> {
     return of(createTestEntryCollection());
   }
 }
