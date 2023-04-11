0e414f1f3ac38045b00fd7ab626728701a6cdf8f
marcNJC <marc.torres@njcode.es>
2023-04-11 13:03:49 +0200

    refactor: updating sitemap url to include lang

--- server/root/root.service.ts
+++ server/root/root.service.ts
@@ -8,17 +8,20 @@ import {
 @Injectable()
 export class RootService {
   async getSitemap(entries: EntryCollectionWithLinkResolutionAndWithUnresolvableLinks<TypePageFields>, originUrl: string): Promise<Buffer> {
+    const available_langs = process.env['AVAILABLE_LANGS']!.split(',');
     const sitemapStream = new SitemapStream({
       hostname: originUrl,
     });
 
     entries.items.forEach(entry => {
-      sitemapStream.write({
-        changefreq: EnumChangefreq.MONTHLY,
-        lastmod: entry.sys.updatedAt,
-        priority: 1.0,
-        url: entry.fields.slug,
-      } as SitemapItem);
+      available_langs.forEach(lang => {
+        sitemapStream.write({
+          changefreq: EnumChangefreq.MONTHLY,
+          lastmod: entry.sys.updatedAt,
+          priority: 1.0,
+          url: `${lang}${entry.fields.slug}`,
+        } as SitemapItem);
+      });
     });
 
     sitemapStream.end();
