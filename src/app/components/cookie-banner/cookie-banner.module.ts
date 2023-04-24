3c6791a60882d36ad9740d1be1fc46cf99e32165
marcNJC <marc.torres@njcode.es>
2023-04-24 12:27:26 +0200

    refactor(i18n): adding internalization to cookie modal

--- src/app/components/cookie-banner/cookie-banner.module.ts
+++ src/app/components/cookie-banner/cookie-banner.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CookieBannerComponent } from '@components/cookie-banner/cookie-banner.component';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CookieBannerComponent } from '@components/cookie-banner/cookie-banner.c
   ],
   imports: [
     CommonModule,
+    TranslocoModule,
   ],
   exports: [
     CookieBannerComponent,
