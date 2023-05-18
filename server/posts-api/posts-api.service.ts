37a9b30d7a00a2f2f41153bb736a11a401e77a7d
marcNJC <marc.torres@njcode.es>
2023-05-18 17:32:22 +0200

    feat(post-api): started using xml2js library

--- server/posts-api/posts-api.service.ts
+++ server/posts-api/posts-api.service.ts
@@ -1,22 +1,60 @@
 import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
-import { Posts } from '@server/models/posts';
+import { Post } from '@server/models/post';
+import { parseStringPromise } from 'xml2js';
 import {
-  catchError, map, Observable, of,
+  catchError, Observable, of, switchMap,
 } from 'rxjs';
 
 @Injectable()
 export class PostsApiService {
-  constructor(@Inject(HttpService) private readonly httpService: HttpService) {}
+  private readonly RSSU = 'https://medium.com/feed/@';
 
-  getPosts(user: string): Observable<Posts> {
-    const RSSUrl = `https://medium.com/feed/@${user}`;
-    const RSSConverter = `https://api.rss2json.com/v1/api.json?rss_url=${RSSUrl}`;
-    return this.httpService.get<Posts>(RSSConverter, {
-      headers: { 'Accept-Encoding': 'application/json' },
+  constructor(@Inject(HttpService) readonly httpService: HttpService) {}
+
+  getPosts(user: string): Observable<Post[]> {
+    const xmlUrl = `${this.RSSU}${user}`;
+    const Posts = this.httpService.get<string>(xmlUrl, {
+      headers: { 'Accept-Encoding': 'application/xml' },
     }).pipe(
-      map(response => response.data),
-      catchError(() => of({ } as Posts)),
+      switchMap(response => this.getContent(response.data)),
+      catchError(() => of(null as unknown as Post[])),
     );
+    return Posts;
+  }
+
+  private async getContent(xmlString: string):Promise<Post[]> {
+    const posts = await parseStringPromise(xmlString, {
+      tagNameProcessors: [this.normalizeTagsName],
+      valueProcessors: [this.getThumbnail],
+    }).then(result => Promise.resolve(result.rss.channel[0].item as Post[]))
+      .catch(error => Promise.reject(error));
+    return posts;
+  }
+
+  private normalizeTagsName(name: string): string {
+    const searchContentName = 'content:encoded';
+    const searchCategoriesName = 'category';
+    const desiredContentName = 'thumbnail';
+    const desiredCategoriesName = 'categories';
+
+    switch (name) {
+      case searchContentName:
+        return desiredContentName;
+      case searchCategoriesName:
+        return desiredCategoriesName;
+      default:
+        return name;
+    }
+  }
+
+  private getThumbnail(name: string): string {
+    const regex = /<img[^>]+src="([^">]+)"/;
+    const match = name.match(regex);
+
+    if (match) {
+      return match[1];
+    }
+    return name;
   }
 }
