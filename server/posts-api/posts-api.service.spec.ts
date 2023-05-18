7fb2eace8fef9bdd57d3b62f5fa3a719c7589845
marcNJC <marc.torres@njcode.es>
2023-05-18 17:29:59 +0200

    refactor(post-api): added testing

--- server/posts-api/posts-api.service.spec.ts
+++ server/posts-api/posts-api.service.spec.ts
@@ -1,6 +1,8 @@
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Post } from '@server/models/post';
 import { PostsApiService } from '@server/posts-api/posts-api.service';
+import { postApiStub } from '@server/shared/testing/stub/post-api.stub';
 
 describe('PostsApiService', () => {
   let service: PostsApiService;
@@ -9,6 +11,10 @@ describe('PostsApiService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PostsApiService,
+        {
+          provide: HttpService,
+          useClass: postApiStub,
+        },
       ],
       imports: [
         HttpModule,
@@ -19,5 +25,32 @@ describe('PostsApiService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('#getPosts', () => {
+    it('should return proper post[]', async () => {
+      const user = 'test';
+
+      interface testObject {
+        title: string[];
+        link: string[];
+        pubDate: string[];
+        thumbnail: string[];
+        categories: string[];
+      }
+
+      const PostsJson: testObject[] = [
+        {
+          title: ['test'],
+          link: ['test'],
+          pubDate: ['1995-12-17T02:24:00.000Z'],
+          thumbnail: ['https://cdn-images-1.medium.com/max/1024/1*JscDkmNIPwBxvvbU_K_uxA.png'],
+          categories: ['test'],
+        },
+      ];
+      service.getPosts(user).subscribe(posts => {
+        expect(posts).toEqual(PostsJson as unknown as Post[]);
+      });
+    });
   });
 });
---
+++ server/shared/testing/snapshots/medium.snapshot.xml.ts
@@ -0,0 +1,5 @@
+export function mediumSnapshot(): string {
+  return '<rss><channel><item><title>test</title><link>test</link><category>test</category>'
+  + '<content:encoded>https://cdn-images-1.medium.com/max/1024/1*JscDkmNIPwBxvvbU_K_uxA.png</content:encoded>'
+  + '<pubDate>1995-12-17T02:24:00.000Z</pubDate></item></channel></rss>';
+}
