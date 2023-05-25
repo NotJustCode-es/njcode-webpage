bce49ef1baa4bbed1e02837503b2801d7f8c3f69
Jose Ignacio Rubio <jrubiosauceda@gmail.com>
2023-05-25 10:26:55 +0200

    refactor(link): remove div and conditional title

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   i18n: {
-    defaultLang: 'en',
+    defaultLang: 'es',
     availableLangs: ['en', 'es'],
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   i18n: {
-    defaultLang: 'en',
+    defaultLang: 'es',
     availableLangs: ['en', 'es'],
   },
 };
