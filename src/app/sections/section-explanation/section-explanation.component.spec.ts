db0a03568c355b7ef80dfb89734fdb378e558db0
marcNJC <marc.torres@njcode.es>
2023-07-12 17:43:28 +0200

    refactor(sections): updating sections

--- src/app/sections/section-explanation/section-explanation.component.spec.ts
+++ src/app/sections/section-explanation/section-explanation.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SectionExplanationComponent } from '@sections/section-explanation/section-explanation.component';
 import { createTestEntry } from '@shared/testing/utils/contentful.utils';
+import { Asset } from 'contentful';
 
 describe('SectionExplanationComponent', () => {
   let component: SectionExplanationComponent;
@@ -23,6 +24,7 @@ describe('SectionExplanationComponent', () => {
         title: 'test',
         description: 'test',
         checklist: ['test'],
+        workflowImage: null as unknown as Asset,
       }),
       imageOnLeftSide: false,
     };
