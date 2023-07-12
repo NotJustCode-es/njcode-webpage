4b8f3ffa32a18985a0758f9e5dc48ac63668e841
marcNJC <marc.torres@njcode.es>
2023-07-12 17:42:56 +0200

    refactor(section-process): using work flow component

--- src/app/sections/section-process/section-process.module.ts
+++ src/app/sections/section-process/section-process.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SectionProcessComponent } from './section-process.component';
+import { WorkFlowModule } from '@components/work-flow/work-flow.module';
+import { SectionProcessComponent } from '@sections/section-process/section-process.component';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { SectionProcessComponent } from './section-process.component';
   ],
   imports: [
     CommonModule,
+    WorkFlowModule,
   ],
   exports: [
     SectionProcessComponent,
