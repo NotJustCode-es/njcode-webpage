11808014b0e6ce328c926459e49faf33d7f5d41e
marcNJC <marc.torres@njcode.es>
2023-07-21 16:03:13 +0200

    feat(section-highlights): adding optional title

--- server/models/contentful-content-types/section-highlights.ts
+++ server/models/contentful-content-types/section-highlights.ts
@@ -5,6 +5,7 @@ export interface TypeSection__highlightsFields {
   name: Contentful.EntryFields.Symbol;
   highlights: Contentful.Entry<TypeHighlightFields>[];
   centered: Contentful.EntryFields.Boolean;
+  title?: Contentful.EntryFields.Symbol;
 }
 
 export type TypeSection__highlights = Contentful.Entry<TypeSection__highlightsFields>;
