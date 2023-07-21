ba95e82b19fbe80da9257f8714ddf376502acd9a
marcNJC <marc.torres@njcode.es>
2023-07-21 15:59:41 +0200

    feat(section-hero): adding optional body long text

--- server/models/contentful-content-types/section-hero.ts
+++ server/models/contentful-content-types/section-hero.ts
@@ -3,7 +3,8 @@ import * as Contentful from 'contentful';
 export interface TypeSection__heroFields {
   name: Contentful.EntryFields.Symbol;
   title: Contentful.EntryFields.Symbol;
-  description: Contentful.EntryFields.Symbol;
+  description?: Contentful.EntryFields.Symbol;
+  body?: Contentful.EntryFields.Text
 }
 
 export type TypeSection__hero = Contentful.Entry<TypeSection__heroFields>;
