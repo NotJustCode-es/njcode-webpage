6ecf2f2d53e9138121281773513ae836745d1f1d
marcNJC <marc.torres@njcode.es>
2023-07-24 16:50:14 +0200

    refactor(section-explanation-collapsed): adding link

--- server/models/contentful-content-types/entry.ts
+++ server/models/contentful-content-types/entry.ts
@@ -3,7 +3,7 @@ import * as Contentful from 'contentful';
 export interface TypeEntryFields {
   name: Contentful.EntryFields.Symbol;
   title: Contentful.EntryFields.Symbol;
-  body: Contentful.EntryFields.Symbol;
+  text: Contentful.EntryFields.Text;
 }
 
 export type TypeEntry = Contentful.Entry<TypeEntryFields>;
--- server/models/contentful-content-types/section-explanation-collapsed.ts
+++ server/models/contentful-content-types/section-explanation-collapsed.ts
@@ -1,4 +1,5 @@
 import { TypeEntryFields } from '@server/models/contentful-content-types/entry';
+import { TypeRouteLinkFields } from '@server/models/contentful-content-types/route-link';
 import * as Contentful from 'contentful';
 
 export interface TypeSection__explanation__collapsedFields {
@@ -6,6 +7,8 @@ export interface TypeSection__explanation__collapsedFields {
   title: Contentful.EntryFields.Symbol;
   entries: Contentful.Entry<TypeEntryFields>[];
   image?: Contentful.Asset;
+  linkText: Contentful.EntryFields.Symbol;
+  link: Contentful.Entry<TypeRouteLinkFields>;
 }
 
 export type TypeSection__explanation_collapsed = Contentful.Entry<TypeSection__explanation__collapsedFields>;
